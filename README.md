b6dd86f0e5c5c648ca4f69e7ece73e561fdc7e66
Alain Ravet <alainravet@gmail.com>
2012-06-16 16:05:50 -0700

    INIT : appscrolls new the_oauth_grid --scrolls git env_yaml guard jquery rails_basics rspec twitter_bootstrap simple_form sqlite3

---
+++ README.md
@@ -0,0 +1,25 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* env_yaml
+* git
+* guard
+* jquery
+* rails_basics
+* rspec
+* sqlite3
+* twitter_bootstrap
+* simple_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheOauthGrid::Application.config.secret_token = '1b4439063f1db0889b0434d362ce6ab8c2f57631d7a8004bc050f9f38a7d56dadc939367df2aeb231d099f5ec00d23d5436be004f10b216464043ce178dab6cb'
