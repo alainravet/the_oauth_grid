b7916b264f909ac4e605f2c645825d907c312cdf
Alain Ravet <alainravet@gmail.com>
2012-06-17 13:39:57 -0700

    refactor and cleanup

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,25 +1,43 @@
 class TwitterController < ApplicationController
 
   def check_credentials
-    @authentication = Authentication.find(params[:authentication_id])
+    authentication = Authentication.find(params[:authentication_id])
 
-    Twitter.configure do |config|
-      config.consumer_key     = CREDENTIALS_STORE['twitter']['consumer_key'   ]
-      config.consumer_secret  = CREDENTIALS_STORE['twitter']['consumer_secret']
-      config.oauth_token        = @authentication.token
-      config.oauth_token_secret = @authentication.secret
-    end
+    load_twitter_application_credentials(CREDENTIALS_STORE['twitter'])
+    client = twitter_client_with_credentials(authentication)
 
     begin
-      Twitter.verify_credentials
+      client.verify_credentials
       flash[:notice] = 'Your Twitter credentials are valid'
 
     rescue Twitter::Error::Unauthorized => e
-      @authentication.delete  # remove invalid credentials from DB
+      authentication.delete  # remove invalid credentials from DB
       flash[:error] = 'Error : Your Twitter credentials are invalid'
+
+    rescue => e # see **1 below
+      flash[:error] = "Unexpected Error : #{e}"
     end
 
     redirect_to root_path, :error => 'Error : Your Twitter credentials are invalid'
   end
 
+private
+
+  def load_twitter_application_credentials(store)
+    Twitter.configure do |config|
+      config.consumer_key     = store['consumer_key'   ]
+      config.consumer_secret  = store['consumer_secret']
+    end
+  end
+
+  def twitter_client_with_credentials(authentication)
+    Twitter::Client.new(:oauth_token => authentication.token, :oauth_token_secret => authentication.secret)
+  end
+
 end
+
+__END__
+
+**1 : NETWORK ERROR encountered a few times
+  Faraday::Error::ConnectionFailed in TwitterController#check_credentials
+    getaddrinfo: nodename nor servname provided, or not known
