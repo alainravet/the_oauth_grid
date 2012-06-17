7b0c0ac70291c82061e56bffa8969ebb403e006b
Alain Ravet <alainravet@gmail.com>
2012-06-16 18:30:01 -0700

    store the providers oAuth credentials in CREDENTIALS_STORES

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 
+CREDENTIALS_STORE = YAML.load(File.open(File.dirname(__FILE__) + '/../credentials.yml'))[Rails.env]
+
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['consumer_key'], ENV['consumer_secret']
+  provider :twitter, CREDENTIALS_STORE['twitter']['consumer_key'], CREDENTIALS_STORE['twitter']['consumer_secret']
 end
