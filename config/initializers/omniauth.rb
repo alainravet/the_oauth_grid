f2f8ec7d3ee6501b2d0013edcee360602916aa61
Alain Ravet <alainravet@gmail.com>
2012-06-17 18:44:16 -0700

    hook Facebook into omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 
 CREDENTIALS_STORE = YAML.load(File.open(File.dirname(__FILE__) + '/../credentials.yml'))[Rails.env]
 
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, CREDENTIALS_STORE['twitter']['consumer_key'], CREDENTIALS_STORE['twitter']['consumer_secret']
+  provider :twitter,  CREDENTIALS_STORE['twitter' ]['consumer_key'], CREDENTIALS_STORE['twitter' ]['consumer_secret']
+  provider :facebook, CREDENTIALS_STORE['facebook']['consumer_key'], CREDENTIALS_STORE['facebook']['consumer_secret'],
+           :scope => 'email,user_birthday,read_stream'
+
 end
